c513e2abeb05da84300e6ac381a45f254846a671
Patrick Vogt <patrick.vogt@unibas.ch>
2020-01-02 12:38:30 +0100

    synthesize speach

---
+++ reden.py
@@ -0,0 +1,16 @@
+from pybricks import ev3brick as brick
+from pybricks.ev3devices import (Motor, TouchSensor, ColorSensor, InfraredSensor, UltrasonicSensor, GyroSensor)
+from pybricks.parameters import (Port, Stop, Direction, Button, Color, SoundFile, ImageFile, Align)
+from pybricks.tools import print, wait, StopWatch
+import sys 
+import os
+
+class Reden():
+
+    @staticmethod
+    def speak(msg:text, german=True):
+        lang=""
+        if german:
+            lang="-vde"
+        os.system("/usr/bin/espeak -a 200 --stdout '{}' {} > out.wav".format(msg,lang))
+        brick.sound.file("out.wav",volume=100)
