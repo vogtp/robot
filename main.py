4cacdf6149ec28a409cabc3507e06ab303d98396
Patrick Vogt <patrick.vogt@unibas.ch>
2020-01-02 14:03:48 +0100

    experiments

--- main.py
+++ main.py
@@ -17,8 +17,6 @@ from reden import Reden
 
 baseSpeed=50
 
-Reden.speak("Hallo Andrin und Lauri")
-
 #sys.exit()
 
 
@@ -29,10 +27,13 @@ def lineFollower():
     motor_rechts=Motor(Port.B)
     #papiHilfe.wartenAmAnfang()
     targetColor=cs.reflection()
+    Reden.reden("Farbe {}".format(targetColor))
 
     #pid = PID(10,5,2., targetColor, output_limits=(-190,190))
-    pidStear = PID(Kp=-2,Ki=-.0,Kd=-.0, setpoint=targetColor, output_limits=(-60,60))
+    pidStear = PID(Kp=-2,Ki=.0,Kd=-.0, setpoint=targetColor, output_limits=(-60,60))
     pidSpeed = PID(3,1,3, 20, output_limits=(20,100))
+
+    baseSpeed = 30
 
     motor_rechts.dc(50)
     motor_links.dc(50)
@@ -41,30 +42,36 @@ def lineFollower():
 
     print(motor_rechts)
 
-    spd=50
+    spd=30
     while 1:
         color=cs.reflection()
-        error = abs(color - targetColor)
+       # error = abs(color - targetColor)
         stear = pidStear(color)
         #spd = round(pidSpeed(color))
-        if error > 10:
-            spd-=error
-            if spd < 0:
-                if abs(stear) < 5:
-                    spd = 3
-                else:
-                    spd = 0
-        if error > 5:
-            spd-=1
-            if spd < 0:
-                spd = 3
+        # if error > 10:
+        #     spd-=error
+        #     if spd < 0:
+        #         if abs(stear) < 5:
+        #             spd = 3
+        #         else:
+        #             spd = 0
+        # if error > 5:
+        #     spd-=1
+        #     if spd < 0:
+        #         spd = 3
+        # else:
+        #     spd+=1
+        # if spd > baseSpeed:
+        #     spd=baseSpeed
+        if abs(stear) > 20:
+            motor_rechts.dc(stear)
+            motor_links.dc(0)
         else:
-            spd+=1
-        if spd > 50:
-            spd=50
-        print("stear {} speed {} color {} target {} error {}".format(stear, spd, color, targetColor,error))
-        motor_rechts.dc(spd+stear)
-        motor_links.dc(spd-stear)
+            motor_rechts.dc(spd+stear)
+            motor_links.dc(spd)
+        
+        print("stear {} speed {} color {} target {} ".format(stear, spd, color, targetColor))
+        
 
 def lineFollowerRobot():
     cs = PortHelper.getSensor(ColorSensor)
@@ -74,7 +81,7 @@ def lineFollowerRobot():
     targetColor=cs.reflection()
 
     #pid = PID(10,5,2., targetColor, output_limits=(-190,190))
-    pidStear = PID(Kp=4,Ki=-0.1,Kd=-0.1, setpoint=targetColor, output_limits=(-120,120))
+    pidStear = PID(Kp=5,Ki=-0.,Kd=0.1, setpoint=targetColor, output_limits=(-120,120))
     pidSpeed = PID(3,1,3, 20, output_limits=(20,100))
 
     spd=50
@@ -83,12 +90,16 @@ def lineFollowerRobot():
         error = abs(color - targetColor)
         stear = round(pidStear(color))
         #spd = round(pidSpeed(color))
-        if error > 10:
-            spd-=error
-            if spd < 20:
-                spd = 20
-        elif error < 2:
-            spd+=5
+        # if error > 10:
+        #     spd-=error
+        #     if spd < 20:
+        #         spd = 20
+        # elif error < 2:
+        #     spd+=5
+        if abs(stear) > 10:
+            spd=0
+        else:
+            spd=30
         print("stear {} speed {} color {} target {} error {}".format(stear, spd, color, targetColor,error))
         roboter.drive(stearing=stear, speed=spd)
     
@@ -98,7 +109,7 @@ def seeker():
     speed = Speed(roboter=roboter)
     speed.start()
     while 1:
-        drive()
+        roboter.drive()
         if speed():
             brick.sound.beep()
             roboter.ausweichen()
@@ -134,4 +145,29 @@ def seeker():
         wait(10)
 
 
-lineFollower()
+auswahl=[ (lineFollowerRobot, "linie als roboter"), (lineFollower, "linie mit Motor"), (seeker, "rum fahren")]
+
+#lineFollower()
+
+i=0
+while 1:
+    if i < 0:
+        i = len(auswahl)
+    if not i < len(auswahl):
+        i=0
+    code, name = auswahl[i]
+    Reden.reden(name)
+    btn = []
+    while not any(btn):
+        btn = brick.buttons()
+        if Button.CENTER in btn:
+            Reden.reden(name)
+            code()
+        if Button.LEFT in btn:
+            i+=1
+            break
+        if Button.RIGHT in btn:
+            i-=1
+            break
+        wait(50)
+#lineFollower()
